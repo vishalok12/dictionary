c29241a8269900740cf58844abdfcfc6e6e639ab
Vishal Kumar <vishal.rgiit@gmail.com>
2013-07-10 06:31:18 +0530

    changes to run app in heroku

--- server.js
+++ server.js
@@ -129,7 +129,7 @@ app.delete( '/api/words/:id', function( request, response ) {
 });
 
 //Start server
-var port = 3000;
+var port = process.env.PORT || 3000;
 app.listen( port, function() {
     console.log( 'Express server listening on port %d in %s mode', port, app.settings.env );
 });
